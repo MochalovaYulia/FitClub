21363c15ab085b435d69fa2934ca7c83d60cbbb2
Yulia <Yulia.Mochalova.2001@mail.ru>
2023-07-20 23:08:58 +0300

    plans component

---
+++ src/components/Plans/Plans.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import './Plans.css'
+import {plansData} from '../../data/plansData'
+import whiteTick from '../../assets/whiteTick.png'
+
+export const Plans = () => {
+  return (
+    <div className='plans-container'>
+        <div className='programs-header'>
+            <span className='stroke-text'>ready to start</span>
+            <span>your journey</span>
+            <span className='stroke-text'>now withus</span>
+        </div>
+        <div className='plans'>
+            {plansData.map((plan) => (
+                <div key={plan.name} className='plan'>
+                    {plan.icon}
+                    <span>{plan.name}</span>
+                    <span> $ {plan.price}</span>
+
+                    <div className='features'>
+                        {plan.features.map((feature) => (
+                            <div key={feature} className='feature'>
+                                <img src={whiteTick} alt=''/>
+                                <span>{feature}</span>
+                            </div>
+                        ))}
+                    </div>
+
+                    <div>
+                        <span>See more benefits -</span>
+                    </div>
+                    <button className='btn'>Join now</button>
+                    
+                </div>
+            ))}
+        </div>
+    </div>
+  )
+}
