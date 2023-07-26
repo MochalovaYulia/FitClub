28329c050f49b18706150006f304adabda7e3eb0
Yulia <Yulia.mochalova.2001@mail.ru>
2023-07-26 22:02:08 +0300

    adding animation

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,81 +5,103 @@ import hero_image from '../../assets/hero_image.png'
 import hero_image_back from '../../assets/hero_image_back.png'
 import heart from '../../assets/heart.png'
 import calories from '../../assets/calories.png'
+import { motion } from 'framer-motion'
 
 export const Hero = () => {
-  return (
-    <div className='hero'>
-        <div className='left-h'>
+    const transition = { type: 'string', duration: 3 }
+    return (
+        <div className='hero'>
+            <div className='left-h'>
 
-            <div className='blur hero-blur'></div>
-            <Header />
+                <div className='blur hero-blur'></div>
+                <Header />
 
-            {/* the-best-ad */}
-            <div className='the-best-ad'>
-                <div></div>
-                <span>The best fitness club in the town</span>
+                {/* the-best-ad */}
+                <div className='the-best-ad'>
+                    <motion.div
+                        initial={{left:'238px'}}
+                        whileInView={{left:'8px'}}
+                        transition={transition}
+                    ></motion.div>
+                    <span>The best fitness club in the town</span>
+                </div>
+
+                {/* hero header */}
+                <div className='hero-text'>
+                    <div>
+                        <span className='stroke-text'>Shape </span>
+                        <span>Your</span>
+                    </div>
+                    <div>
+                        <span>Ideal body</span>
+                    </div>
+                    <div>
+                        <span className='span'>
+                            In here we will help you to shape snd build your ideal body and life up your life to fullest
+                        </span>
+                    </div>
+                </div>
+
+                {/* figures */}
+                <div className='figures'>
+                    <div>
+                        <span className='figures-num'>+140</span>
+                        <span className='figures-text'>exprt couchs</span>
+                    </div>
+                    <div>
+                        <span className='figures-num'>+978</span>
+                        <span className='figures-text'>members jouned</span>
+                    </div>
+                    <div>
+                        <span className='figures-num'>+50</span>
+                        <span className='figures-text'>fitness programs</span>
+                    </div>
+                </div>
+
+                {/* hero button */}
+                <div className='hero-buttons'>
+                    <button className='btn'>Get Started</button>
+                    <button className='btn'>Learn More</button>
+                </div>
             </div>
+            <div className='right-h'>
+                <button className='btn'>Join Now</button>
 
-            {/* hero header */}
-            <div className='hero-text'>
-                <div>
-                    <span className='stroke-text'>Shape </span>
-                    <span>Your</span>
-                </div>
-                <div>
-                    <span>Ideal body</span>
-                </div>
-                <div>
-                    <span className='span'>
-                        In here we will help you to shape snd build your ideal body and life up your life to fullest
-                    </span>
-                </div>
-            </div>
+                <motion.div 
+                    className='heart-rate'
+                    initial={{right:'-1rem'}}
+                    whileInView={{right:'4rem'}}
+                    transition={transition}
+                >
+                    <img src={heart} alt='' />
+                    <span>Heart Rate</span>
+                    <span>116 bpm</span>
+                </motion.div>
 
-            {/* figures */}
-            <div className='figures'>
-                <div>
-                    <span className='figures-num'>+140</span>
-                    <span className='figures-text'>exprt couchs</span>
-                </div>
-                <div>
-                    <span className='figures-num'>+978</span>
-                    <span className='figures-text'>members jouned</span>
-                </div>
-                <div>
-                    <span className='figures-num'>+50</span>
-                    <span className='figures-text'>fitness programs</span>
-                </div>
-            </div>
+                {/* hero img */}
+                <img className='hero-img' src={hero_image} alt='' />
+                <motion.img
+                    initial={{right:'11rem'}}
+                    whileInView={{right:'20rem'}}
+                    transition={transition} 
+                    className='hero-img-back' 
+                    src={hero_image_back} alt='' 
+                />
 
-            {/* hero button */}
-            <div className='hero-buttons'>
-                <button className='btn'>Get Started</button>
-                <button className='btn'>Learn More</button>
+                {/* calories */}
+                <motion.div 
+                    className='calories'
+                    initial={{right:'37rem'}}
+                    whileInView={{right:'28rem'}}
+                    transition={transition}
+                >
+                    <img src={calories} alt='' />
+                    <div>
+                        <span>Calories Burned</span>
+                        <span>220 kcal</span>
+                    </div>
+                </motion.div>
             </div>
         </div>
-        <div className='right-h'>
-            <button className='btn'>Join Now</button>
-
-            <div className='heart-rate'>
-                <img src={heart} alt=''/>
-                <span>Heart Rate</span>
-                <span>116 bpm</span>
-            </div>
-
-            {/* hero img */}
-            <img className='hero-img' src={hero_image} alt=''/>
-            <img className='hero-img-back' src={hero_image_back} alt=''/>
-
-            {/* calories */}
-            <div className='calories'>
-                <img src={calories} alt=''/>
-                <div>
-                    <span>Calories Burned</span>
-                    <span>220 kcal</span>
-                </div>
-            </div>
-        </div>
-    </div>
-  )
+    )
 }
--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -3,10 +3,12 @@ import './Testimonials.css'
 import { testimonialsData } from '../../data/testimonialsData'
 import leftArrow from '../../assets/leftArrow.png'
 import rightArrow from '../../assets/rightArrow.png'
+import { motion } from 'framer-motion'
 
 export const Testimonials = () => {
     const [selected, setSelected] = useState(0);
     const tLength = testimonialsData.length;
+    const transition = {type: 'string', duration: 3}
 
     return (
         <div className='Testimonials'>
@@ -14,18 +16,37 @@ export const Testimonials = () => {
                 <span>Testimonials</span>
                 <span className='stroke-text'>what they</span>
                 <span>say about us</span>
-                <span>
+                <motion.span
+                    key={selected} 
+                    initial={{opacity: 0, x: -100}}
+                    whileInView={{opacity: 1, x : 0}}
+                    transition={transition}
+                >
                     {testimonialsData[selected].review}
-                </span>
+                </motion.span>
                 <span>
                     <span className='span-name'>{testimonialsData[selected].name}</span>{" "}
                     - {testimonialsData[selected].status}
                 </span>
             </div>
             <div className='right-t'>
-                <div></div>
-                <div></div>
-                <img src={testimonialsData[selected].image} alt='' />
+                <motion.div
+                    initial={{opacity: 0, x: -100}}
+                    whileInView={{opacity: 1, x : 0}}
+                    transition={transition}
+                ></motion.div>
+                <motion.div
+                    initial={{opacity: 0, x: 100}}
+                    whileInView={{opacity: 1, x : 0}}
+                    transition={transition}
+                ></motion.div>
+                <motion.img 
+                    key={selected} 
+                    initial={{opacity: 0, x: 100}}
+                    animate={{opacity: 1, x : 0}}
+                    exit={{opacity: 0, x: -100}}
+                    transition={{...transition, duration: 1}}
+                    src={testimonialsData[selected].image} alt='' />
                 <div className='arrows'>
                     <img
                         onClick={() => {
